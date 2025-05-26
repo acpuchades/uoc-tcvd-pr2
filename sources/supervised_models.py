ad4dcc3080a224b52172ee217a5c07f2915b9bcb
acpuchades <59510094+acpuchades@users.noreply.github.com>
2025-05-26 23:08:30 +0200

    Include plots in standalone scripts

--- sources/supervised_models.py
+++ sources/supervised_models.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
-from idealista_data import X_train, y_train, X_test, y_test
-from data_pipeline import preprocessor, boxcox_transform, inv_boxcox_transform
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 from sklearn.pipeline import make_pipeline
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.compose import TransformedTargetRegressor
 from sklearn.ensemble import HistGradientBoostingRegressor
 from sklearn.metrics import root_mean_squared_error, r2_score
+
+from idealista_data import X_train, y_train, X_test, y_test
+from data_pipeline import preprocessor, boxcox_transform, inv_boxcox_transform
 
 dt_model = make_pipeline(
   preprocessor,
@@ -43,3 +46,21 @@ y_pred = gbr_model_t.predict(X_test)
 
 print("RMSE:", root_mean_squared_error(y_test, y_pred))
 print("R²:", r2_score(y_test, y_pred))
+
+plt.figure(figsize=(6, 4))
+plt.scatter(x=y_pred, y=y_test, s=3, alpha=0.2)
+plt.axline((0, 0), (1, 1), color='gray', linestyle='--')
+plt.xlabel("Predicción del precio/m2")
+plt.ylabel("Precio/m2 real")
+plt.show()
+
+y_resid = y_test - y_pred
+
+plt.figure(figsize=(6, 4))
+sns.scatterplot(x=y_pred, y=y_resid, s=3, alpha=0.5)
+plt.axhline(0, color='gray', linestyle='--')
+plt.xlabel("Predicted price/m²")
+plt.ylabel("Residual")
+plt.title("Residuals vs. Predicted")
+plt.tight_layout()
+plt.show()
--- sources/unsupervised_models.py
+++ sources/unsupervised_models.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 from sklearn.pipeline import make_pipeline
 
@@ -12,3 +13,16 @@ tsne_model = make_pipeline(
 )
 
 tsne_results = tsne_model.fit_transform(X_train)
+
+plt.figure(figsize=(8, 6))
+plt.scatter(tsne_results[:, 0], tsne_results[:, 1], s=1, alpha=0.5)
+plt.title("t-SNE visualization of property features")
+plt.xlabel("t-SNE Component 1")
+plt.ylabel("t-SNE Component 2")
+plt.tight_layout()
+plt.show()
+
+plt.figure(figsize=(8, 6))
+sns.scatterplot(X_train, x=tsne_results[:, 0], y=tsne_results[:, 1],
+                hue='ciudad', palette='Set1', s=3, alpha=0.5, legend=False)
+plt.show()
